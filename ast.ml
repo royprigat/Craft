6275e39dd8ebdf8ef5c5d7bac660b5c6273c1a68
Roy Prigat <rp2719@columbia.edu>
2017-11-07 15:17:05 -0500

    ast for helloworld

--- ast.ml
+++ ast.ml
@@ -1,9 +1,7 @@
 (* CRAFT Abstract Syntax Tree and functions for printing it *)
 
 type op = Add | Sub | Mult | Div | Equal | Neq | Less | Leq | Greater | Geq | And | Or | Coll
-
 type uop = Neg | Not
-
 type typ = Int | Float | Bool | Void
 
 type bind = typ * string
@@ -15,7 +13,8 @@ type expr =
   | Id of string
   | Binop of expr * op * expr
   | Unop of uop * expr
-  | Assign of string * expr
+  | Asgn of string * expr
+  | Accs of string * string
   | Call of string * expr list
   | Noexpr
 
@@ -32,8 +31,8 @@ type stmt =
 type func_decl = {
     typ : typ;
     fname : string;
-    formals : bind list;
-    locals : bind list;
+    formals : typ * string list;
+    locals : typ * string list;
     body : stmt list;
   }
 
@@ -44,17 +43,14 @@ type elem_decl = {
   }
 
 (* Events *)
-type evnt_decl = {
+(* type evnt_decl = {
     evname : string;
     args : bind list;
     body : stmt list;
-  }
+  } *)
 
 (* World/Game *)
 type world_decl = {
-    wname : string;
-    wmembers : var_decl list;
-    init_mem: var_decl list;
     init_body : stmt list;
   }
 
@@ -63,7 +59,7 @@ type program = bind list * func_decl list * elem_decl list * evnt_decl list * wo
 
 (* Pretty-printing functions *)
 
-let string_of_op = function
+(* let string_of_op = function
     Add -> "+"
   | Sub -> "-"
   | Mult -> "*"
@@ -76,7 +72,7 @@ let string_of_op = function
   | Geq -> ">="
   | And -> "&&"
   | Or -> "||"
-  | Collision -> "!!"
+  | Coll -> "!!"
 
 let string_of_uop = function
     Neg -> "-"
@@ -84,6 +80,7 @@ let string_of_uop = function
 
 let rec string_of_expr = function
     Literal(l) -> string_of_int l
+  | FLiteral(f) -> string_of_float f
   | BoolLit(true) -> "true"
   | BoolLit(false) -> "false"
   | Id(s) -> s
@@ -91,6 +88,7 @@ let rec string_of_expr = function
       string_of_expr e1 ^ " " ^ string_of_op o ^ " " ^ string_of_expr e2
   | Unop(o, e) -> string_of_uop o ^ string_of_expr e
   | Assign(v, e) -> v ^ " = " ^ string_of_expr e
+  | Accs(v, e) -> v ^ "." ^ e
   | Call(f, el) ->
       f ^ "(" ^ String.concat ", " (List.map string_of_expr el) ^ ")"
   | Noexpr -> ""
@@ -104,23 +102,39 @@ let rec string_of_stmt = function
   | If(e, s1, s2) ->  "if (" ^ string_of_expr e ^ ")\n" ^
       string_of_stmt s1 ^ "else\n" ^ string_of_stmt s2
   | While(e, s) -> "while (" ^ string_of_expr e ^ ") " ^ string_of_stmt s
-  | Condition(s1, s2) -> 
+  | Condition(s1, s2) -> "condition {"
 
 let string_of_typ = function
     Int -> "int"
+  | Float -> "float"
   | Bool -> "bool"
   | Void -> "void"
+
+let string_of_bind (t,v) = string_of_typ t ^ " " ^ v  
 
 let string_of_vdecl (t, id) = string_of_typ t ^ " " ^ id ^ ";\n"
 
 let string_of_fdecl fdecl =
-  string_of_typ fdecl.typ ^ " " ^
+  "\nfun " ^ string_of_typ fdecl.typ ^ " " ^
   fdecl.fname ^ "(" ^ String.concat ", " (List.map snd fdecl.formals) ^
   ")\n{\n" ^
   String.concat "" (List.map string_of_vdecl fdecl.locals) ^
   String.concat "" (List.map string_of_stmt fdecl.body) ^
   "}\n"
 
+let string_of_evnt  = function
+  Event(e,v,a) ->  string_of_eventCheck e ^ " -> {\n" ^
+   String.concat "" (List.map string_of_vdecl v) ^
+   String.concat "" (List.map string_of_stmt a) ^ "}\n"
+
+let string_of_elemdecl edecl =
+  "\nentity " ^ edecl.ename ^ " " ^
+  "{\n" ^
+  String.concat "" (List.map string_of_vdecl edecl.members) ^ 
+  String.concat "" (List.map string_of_event edecl.rules) ^
+  "}\n"
+
 let string_of_program (vars, funcs) =
   String.concat "" (List.map string_of_vdecl vars) ^ "\n" ^
-  String.concat "\n" (List.map string_of_fdecl funcs)
+  String.concat "\n" (List.map string_of_fdecl funcs) ^
+  String.concat "" (List.map string_of_entdecl ents) *)
