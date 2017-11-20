b4d79cb3cec4f3a2a618b5135dea797f4655a1c0
Martin Fagerhus <martin.fagerhus@gmail.com>
2017-11-20 18:37:15 -0500

    don't need shell script to compile executable anymore. All taken care of in craft.ml

--- craft.ml
+++ craft.ml
@@ -1,6 +1,8 @@
 (* Top-level of Craft compiler: scan & parse the input,
    check the resulting AST, generate LLVM IR, and dump the module *)
 
+open Printf
+
 module StringMap = Map.Make(String)
 
 type action = Ast | LLVM_IR | Compile
@@ -24,5 +26,30 @@ let _ =
     Ast -> print_string (Ast.string_of_program ast)
   | LLVM_IR -> print_string (Llvm.string_of_llmodule (Codegen.translate ast))
   | Compile -> let m = Codegen.translate ast in
-    (*Llvm_analysis.assert_valid_module m;*)
-    print_string (Llvm.string_of_llmodule m)
+    Llvm_analysis.assert_valid_module m;
+    (* print_string (Llvm.string_of_llmodule m) *)
+    let arg_index = 
+      if Array.length Sys.argv == 2 then
+        1
+      else
+        2 in
+
+    let crf_file_in = Sys.argv.(arg_index) in 
+    let index = String.rindex crf_file_in '.' in
+    let file_name = String.sub crf_file_in 0 index in
+
+
+    let channel_out = open_out (file_name ^ ".ll") in 
+    fprintf channel_out "%s\n" (Llvm.string_of_llmodule m);
+    close_out channel_out;
+    let ll_name = file_name ^ ".ll" in
+    let s_name = file_name ^ ".s" in
+    let exe_name = file_name ^ ".exe" in
+
+    let command_1 = "llc " ^ ll_name ^ " > " ^ s_name in
+    ignore (Sys.command command_1);
+    let command_2 = "gcc -o " ^ exe_name ^ " " ^ s_name ^ " world.o -L/usr/local/lib -lSDL2" in
+    ignore (Sys.command command_2);
+
+   
+
