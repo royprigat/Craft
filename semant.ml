cfe6519aec0c1304a3d6a7a73cf212fd8a3cd258
DTal <Dt2479@columbia.edu>
2017-12-16 18:26:27 -0500

    Semant extras

--- semant.ml
+++ semant.ml
@@ -33,39 +33,47 @@ let check (funcs, events, elements, world) =
     | Color -> "color"
   in
 
-  let type_of_identifier s =
-    try StringMap.find s symbols
-    with Not_found -> E.UndeclaredId(s)
-  in
+  (* return the type of an ID (check given symbols map) *)
+  let type_of_identifier s m =
+      try StringMap.find s m
+        with Not_found -> raise (E.UndeclaredId(s))
+    in
 
   (* Return the type of an expression or throw an exception *)
-  let rec expr = function
+  let rec expr m arg = function
 	   ILiteral _ -> Int
      | BLiteral _ -> Bool
-     | Id s -> type_of_identifier s
-     | Binop(e1, op, e2) as e -> let t1 = expr e1 and t2 = expr e2 in
+     | FLiteral _ -> Float
+     | Id s -> type_of_identifier s m
+     | Binop(e1, op, e2) as e -> let t1 = expr m arg e1 and t2 = expr m arg e2 in
 	    (match op with
         Add | Sub | Mult | Div when t1 = Int && t2 = Int -> Int
-	       | Equal | Neq when t1 = t2 -> Bool
-	       | Less | Leq | Greater | Geq when t1 = Int && t2 = Int -> Bool
-	       | And | Or when t1 = Bool && t2 = Bool -> Bool
-         | _ -> raise (Failure ("illegal binary operator " ^
-              string_of_typ t1 ^ " " ^ string_of_op op ^ " " ^
-              string_of_typ t2 ^ " in " ^ string_of_expr e))
+        |  Add | Sub | Mult | Div when t1 = Int && t2 = Float -> Float
+		    |  Add | Sub | Mult | Div when t1 = Float && t2 = Int -> Float
+   	    |  Add | Sub | Mult | Div when t1 = Float && t2 = Float -> Float
+	      | Equal | Neq when t1 = t2 -> Bool
+	      | Less | Leq | Greater | Geq when t1 = Int && t2 = Int -> Bool
+	      | Less | Leq | Greater | Geq when t1 = Float && t2 = Float -> Bool
+        | Less | Leq | Greater | Geq when t1 = Float && t2 = Int -> Bool
+        | Less | Leq | Greater | Geq when t1 = Int && t2 = Float -> Bool
+	      | And | Or when t1 = Bool && t2 = Bool -> Bool
+        | _ -> raise (E.IllegalBinOp(string_of_typ t1 ^ " ", string_of_op op ^ " ",
+        string_of_typ t2 ^ " in ", string_of_expr e))
       )
-    | Unop(op, e) as ex -> let t = expr e in
+    | Unop(op, e) as ex -> let t = expr m arg e in
 	   (match op with
 	      Neg when t = Int -> Int
 	       | Not when t = Bool -> Bool
-         | _ -> raise (Failure ("illegal unary operator " ^ string_of_uop op ^
-	  		   string_of_typ t ^ " in " ^ string_of_expr ex))
+         | _ -> raise (E.IllegalUnOp(string_of_uop op, string_of_typ t ^ " in ",
+          string_of_expr ex))
      )
-    | Noexpr -> Void
-    | Assign(var, e) as ex -> let lt = type_of_identifier var
-                              and rt = expr e in
+    (* | Noexpr -> () *)
+    (* | Assign(var, e) as ex -> let lt = type_of_identifier var m
+                              and rt = expr m e in
         check_assign lt rt (Failure ("illegal assignment " ^ string_of_typ lt ^
-				        " = " ^ string_of_typ rt ^ " in " ^ string_of_expr ex))
-    | Call(fname, actuals) as call -> let fd = function_decl fname in
+				        " = " ^ string_of_typ rt ^ " in " ^ string_of_expr ex)) *)
+
+    (* | Call(fname, actuals) as call -> let fd = function_decl fname in
          if List.length actuals != List.length fd.formals then
            raise (Failure ("expecting " ^ string_of_int
              (List.length fd.formals) ^ " arguments in " ^ string_of_expr call))
@@ -75,8 +83,35 @@ let check (funcs, events, elements, world) =
                 (Failure ("illegal actual argument found " ^ string_of_typ et ^
                 " expected " ^ string_of_typ ft ^ " in " ^ string_of_expr e))))
              fd.formals actuals;
-           fd.typ
+           fd.typ*)
+
+    (* | Pr(e1,e2) as e -> let t1 = expr m e1 and t2 = expr m e2 in
+      (match t1 with
+          Int when t2 = Int -> Pair
+          | _ -> raise (E.IllegalPairType("A pair must be made up of two ints"))
+      ) *)
     in
+
+
+    (* Verify a statement or throw an exception *)
+    let rec stmt m arg = function
+    	Block sl -> let rec check_block = function
+        [Return _ as s] -> stmt m arg s
+          | Return _ :: _ -> raise (Failure "nothing may follow a return")
+          | Block sl :: ss -> check_block (sl @ ss)
+          | s :: ss -> stmt m arg s ; check_block ss
+          | [] -> ()
+            in check_block sl
+        | Expr e -> ignore (expr m arg e)
+        (*)| Return e -> let t = expr m arg e in if t = funcs.typ then () else
+             raise (Failure ("return gives " ^ string_of_typ t ^ " expected " ^
+                             string_of_typ func.typ ^ " in " ^ string_of_expr e))
+         | If(p, b1, b2) -> check_bool_expr p m arg; stmt m arg b1; stmt m arg b2
+        | While(p, s) -> check_bool_expr m arg p; stmt m arg s
+        | Return e -> ()*)
+        in
+
+
 
 (* check correct type assignment for "size" *)
 (*  let check_type s t m err =
@@ -152,7 +187,7 @@ in*)
     (* let mems = memTypes world.properties in *)
 
         (* build a map of variables within scope *)
-        let scopeMap =  List.fold_left var StringMap.empty world.properties in
+        let scopeMap =  List.fold_left var StringMap.empty world.w_properties in
 
         (* check members *)
       (*  List.iter (checkVarInit symbols "" ) world.properties;
